ebcd21919498f4d577e5097792dd24998b6c76a0
furkancaglayan <furkancaglayan15@hotmail.com>
2019-08-21 20:33:31 +0300

    Update About.cs

--- Assets/HexWorld/Scripts/Editor/About.cs
+++ Assets/HexWorld/Scripts/Editor/About.cs
@@ -31,13 +31,14 @@ public class About : EditorWindow {
             richText=true,
             fontSize=11
         };
-        GUIStyle urlStyle = new GUIStyle(EditorStyles.miniLabel)
+        uIStyle.wordWrap = true;
+        GUIStyle urlStyle = new GUIStyle()
         {
             richText = true,
-            fontSize = 11
+            fontSize = 12
             
         };
-        GUIStyle logoStyle = new GUIStyle(GUI.skin.window)
+        GUIStyle logoStyle = new GUIStyle(GUI.skin.box)
         {
             padding = new RectOffset(0, 0, 8, 0)
         };
@@ -46,24 +47,50 @@ public class About : EditorWindow {
         GUI.color = new Color(255F / 255F, 211F / 255F, 89F / 255F, .8F);
         GUILayout.BeginHorizontal(logoStyle, GUILayout.Width(position.width), GUILayout.Height(128));
         GUILayout.Space(position.width / 2 - 148);
-
         GUILayout.Label(IconPack.GetBirchgamesLogo(), GUILayout.Width(128), GUILayout.Height(128));
         GUILayout.Label(IconPack.GetHexworldLogo(), GUILayout.Width(128), GUILayout.Height(128));
         GUILayout.EndHorizontal();
-        GUILayout.Label("To find more information about HexWorld, visit <color=green>birchgames.com</color>", uIStyle);
-        GUILayout.EndVertical();
-        Rect rect = EditorGUILayout.GetControlRect();
-        if (Event.current.type == EventType.MouseUp && rect.Contains(Event.current.mousePosition))
-            Application.OpenURL("www.birchgames.com");
-        if (rect.Contains(Event.current.mousePosition))
-        {
-            GUI.Label(rect, "<color=blue>birchgames.com</color>", urlStyle);
-        }
-        else
-        {
-            GUI.Label(rect, "<color=green>birchgames.com</color>", urlStyle);
-        }
-       
+
+
+        GUILayout.Label("BIRCHGAMES",EditorStyles.boldLabel);
+        GUILayout.Label("We are creating assets and games. Check out at: ", uIStyle);
+
+        Link("www.birchgames.com", "birchgames.com", urlStyle);
+     
+
+        GUILayout.Label("For any problem, advice or feature request drop an email: ", uIStyle);
+        Link("mailto:connect@birchgames.com", "connect@birchgames.com", urlStyle);
+        
+        GUILayout.Space(10);
+
+        GUILayout.Label("HexWorld v1.2", EditorStyles.boldLabel);
+        GUILayout.Label("We added some useful guides to HexWorld/Documentation. Check them out to start with HexWorld.", uIStyle);
+        GUILayout.Label("Also, we decided to add users to our private repo to get realtime updates and see the progress." +
+                        "If you are interested in this, drop a mail.", uIStyle);
+        GUILayout.Label("For more info go to:", uIStyle);
+
+
+        Link("https://birchgames.com/hexworld/", "birchgames/hexworld", urlStyle);
+        GUILayout.Label("You can also watch demos on our youtube channel:", uIStyle);
+        Link("https://www.youtube.com/channel/UCpbaG9ieaTReZYD3kaP3U6g","Our Youtube Channel",urlStyle);
+
+        GUILayout.Space(20);
+        GUILayout.Label("Out other social accounts:", uIStyle);
+        Link("https://www.patreon.com/birchgames","Patreon",urlStyle);
+        Link("https://twitter.com/GamesBirch", "Twitter", urlStyle);
+        GUILayout.EndVertical();    
+
         Repaint();
+    }
+
+    private void Link(string url,string name,GUIStyle urlStyle)
+    {
+        Rect rect3 = EditorGUILayout.GetControlRect();
+        if (Event.current.type == EventType.MouseUp && rect3.Contains(Event.current.mousePosition))
+            Application.OpenURL(url);
+        if (rect3.Contains(Event.current.mousePosition))
+            GUI.Label(rect3, "<color=blue>"+ name+"</color>", urlStyle);
+        else
+            GUI.Label(rect3, "<color=green>" + name + "</color>", urlStyle);
     }
 }
